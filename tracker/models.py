2dec8a55d0c76b597081c5bc7cab46fa1623fde8
Blank21-xD <lama.safal21@gmail.com>
2026-04-07 23:36:18 +1000

    Added Skill model, view, and list template

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, default='General')
+    is_learned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Skill
 
-# Create your views here.
+def skill_list(request):
+    # Grab ALL the skills from our "Pantry" (Database)
+    all_skills = Skill.objects.all()
+    
+    # Send them to the "Plate" (HTML Template)
+    return render(request, 'tracker/skill_list.html', {'skills': all_skills})
