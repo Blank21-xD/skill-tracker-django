c8116b6329149a2bdf2bfc8ba3bc211cb0a4b8c2
Blank21-xD <lama.safal21@gmail.com>
2026-04-11 23:45:26 +1000

    Fixed venv issues and completed functional SkillForm with Bootstrap

---
+++ tracker/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Skill
+
+
+class SkillForm(forms.ModelForm):
+    class Meta:  # This must be indented!
+        model = Skill
+        fields = ['name', 'category', 'is_learned']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Skill Name'}),
+            'category': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. Backend'}),
+            'is_learned': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
--- tracker/views.py
+++ tracker/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect  # Added redirect
 from .models import Skill
-from .forms import SkillForm  # Import the form you made yesterday
+from tracker.forms import SkillForm
 
 
 def skill_list(request):
+    # 1. If the user clicked "Submit" (POST)
+    if request.method == "POST":
+        form = SkillForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new skill to the database!
+            return redirect('home')  # Refresh the page to show the new skill
+
+    # 2. If the user is just looking at the page (GET)
     all_skills = Skill.objects.all()
-    form = SkillForm()  # Create a blank form to show on the page
+    form = SkillForm()
 
     return render(request, 'tracker/skill_list.html', {
         'skills': all_skills,
-        'form': form  # Send the form to the template
+        'form': form
     })
