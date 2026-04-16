acffb109a8ca3c815aec2373076243113995174a
Blank21-xD <lama.safal21@gmail.com>
2026-04-16 22:23:44 +1000

    Database: Added priority field to Skill model and ran migrations

--- tracker/models.py
+++ tracker/models.py
@@ -5,6 +5,7 @@ class Skill(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50, default='General')
     is_learned = models.BooleanField(default=False)
+    priority = models.IntegerField(default=1)
 
     class Meta:
 
